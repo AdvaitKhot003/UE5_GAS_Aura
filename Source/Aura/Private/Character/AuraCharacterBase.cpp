10240f8fd7cb6440550bccc5fb9d2e9762487423
AdvaitKhot003 <adwaitkhot@gmail.com>
2025-09-17 17:45:54 +0530

    Player And Enemy Characters Class

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright Advait Khot
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
