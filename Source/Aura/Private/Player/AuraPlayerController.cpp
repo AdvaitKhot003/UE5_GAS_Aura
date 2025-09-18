9d695d0165fc73b1c1b2abbf372f681cc740b5d1
AdvaitKhot003 <adwaitkhot@gmail.com>
2025-09-18 23:56:19 +0530

    Highlight Enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,59 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+	
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	if (LastActor == nullptr) // LastActor is not valid
+	{
+		if (ThisActor != nullptr) // ThisActor is valid
+		{
+			ThisActor->HighlightActor();
+		}
+		else // ThisActor is not valid
+		{
+			// Do Nothing
+		}
+	}
+	else // LastActor is Valid
+	{
+		if (ThisActor == nullptr) // ThisActor is not valid
+		{
+			LastActor->UnHighlightActor();
+		}
+		else // ThisActor is valid
+		{
+			if (LastActor != ThisActor)
+			{
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else // LastActor == ThisActor
+			{
+				// Do Nothing
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
